74f34409243d73e81fa405a7b0147b9c581546e3
Ai_Sama <79479334@qq.com>
2019-08-21 20:54:45 +0800

    Initial commit

---
+++ src/main/java/com/blog/aisamablog/AisamablogApplication.java
@@ -0,0 +1,13 @@
+package com.blog.aisamablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisamablogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AisamablogApplication.class, args);
+    }
+
+}
