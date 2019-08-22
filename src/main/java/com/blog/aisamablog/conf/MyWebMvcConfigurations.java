32552b26c65c161d8cc63a4ee10a4764339b533f
Ai_Sama <79479334@qq.com>
2019-08-22 21:03:53 +0800

    2019-08-22

---
+++ src/main/java/com/blog/aisamablog/conf/MyWebMvcConfigurations.java
@@ -0,0 +1,16 @@
+package com.blog.aisamablog.conf;
+
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @program: aisamablog
+ * @author: ZhangXiangQiang
+ * @create: 2019-08-22 09:52
+ **/
+public class MyWebMvcConfigurations extends WebMvcConfigurationSupport {
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        super.addInterceptors(registry);
+    }
+}
