4b62a64c83713c0ddcebbae9f2bb2a5378cd5042
Ai_Sama <79479334@qq.com>
2019-09-15 21:28:12 +0800

    2019-09-15 1 完善功能

--- src/main/java/com/blog/aisamablog/conf/MyWebMvcConfigurations.java
+++ src/main/java/com/blog/aisamablog/conf/MyWebMvcConfigurations.java
@@ -1,9 +1,11 @@
 package com.blog.aisamablog.conf;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @program: aisamablog
@@ -11,14 +13,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  * @create: 2019-08-22 09:52
  **/
 @Configuration
-public class MyWebMvcConfigurations extends WebMvcConfigurationSupport {
+public class MyWebMvcConfigurations implements WebMvcConfigurer {
 
+    @Value("${img.path}")
+    String path;
     @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
-        super.addInterceptors(registry);
+    public void addInterceptors(InterceptorRegistry registry) {
+
     }
     @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/blogImg/**").addResourceLocations("file:D:/blogImg/");
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/blogImg/**").addResourceLocations("file:"+path);
     }
+    //    @Override
+//    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/blogImg/**").addResourceLocations("file:D:/blogImg/");
+////        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+//        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
 }
--- src/main/java/com/blog/aisamablog/service/BlogContentServiceImpl.java
+++ src/main/java/com/blog/aisamablog/service/BlogContentServiceImpl.java
@@ -6,8 +6,6 @@ import com.blog.aisamablog.model.PageValue;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
